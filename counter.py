d2f5d250347d6aea1637f023f6b844ac0f24e0dc
Sergey Popinevskiy <gurza000@gmail.com>
2017-04-18 18:35:23 +0300

    Calculated bookings. It's number of sold segments in a given PNR.

--- counter.py
+++ counter.py
@@ -8,25 +8,31 @@ from settings import sabre as sabre_settings
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 
 
-def segments(record_locator):
-    """Returns number of segments in a given PNR"""
+def calc_bookings(pnr_record_locator):
+    """Returns number of bookings in a given PNR.
+
+    Number of bookings (sold segments) is multiplication of
+    number of itinerary segments and number of travelers.
+    This function doesn't check tickets information.
+    """
     client = SabreClient(sabre_settings['pos'])
     try:
-        pnr = client.pnr(record_locator)
+        pnr = client.pnr(pnr_record_locator)
         ns = pnr.nsmap
         if None in ns:
             ns['xmlns'] = ns[None]
             ns.pop(None)
         segments = pnr.findall('.//xmlns:ReservationItems//xmlns:FlightSegment', namespaces=ns)
         travelers = pnr.findall('.//xmlns:CustomerInfo/xmlns:PersonName', namespaces=ns)
+        bookings = segments*travelers
     except SabreClientException:
         print client.request_text
         print client.response_text
         raise
-    return pnr
+    return bookings
 
 if __name__ == '__main__':
     from lxml import etree
     pnr = 'YFCDVY'
-    r = segments(pnr)
+    r = calc_bookings(pnr)
     #print etree.tostring(r)
