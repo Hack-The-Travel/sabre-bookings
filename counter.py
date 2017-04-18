3c99b08710f6d0684023985d11e3a23c4be0c830
Sergey Popinevskiy <gurza000@gmail.com>
2017-04-18 17:18:59 +0300

    Added lxml to parse response of Sabre client

--- counter.py
+++ counter.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 import sys
 import logging
+from lxml import etree
 from sabreclient import SabreClient, SabreClientException
 from settings import sabre as sabre_settings
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 
 
-def segments(pnr):
+def segments(record_locator):
     """Returns number of segments in requested PNR"""
     client = SabreClient(sabre_settings['pos'])
     try:
-        return client.pnr(pnr)
+        pnr = client.pnr(record_locator)
     except SabreClientException:
         print client.request_text
         print client.response_text
         raise
+    return pnr
 
 if __name__ == '__main__':
     from lxml import etree
